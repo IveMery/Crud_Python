49a2f0f2b7575cb16927c1ddf8c1eaee9a3876a9
Ivette M <ivette.mery@gmail.com>
2023-07-29 18:17:11 -0400

    agregando tests

--- laboratorio/tests.py
+++ laboratorio/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Laboratorio
 
-# Create your tests here.
+
+class LaboratorioModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Laboratorio.objects.create(nombre='Laboratorio 1', ciudad='Ciudad 1', pais='País 1')
+
+    def test_datos_laboratorio(self):
+        laboratorio = Laboratorio.objects.get(nombre='Laboratorio 1')
+        self.assertEqual(laboratorio.nombre, 'Laboratorio 1')
+        self.assertEqual(laboratorio.ciudad, 'Ciudad 1')
+        self.assertEqual(laboratorio.pais, 'País 1')
+
+
+class InicioTests(TestCase):
+    def test_laboratorios_url_responde_200(self):
+        url = '/laboratorio/mostrar/'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    #reverse
+    def test_laboratorio_url_responde_200(self):
+        url = reverse('lista_laboratorios')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Listado de Laboratorios")
+        self.assertNotContains(response, "No es la Página") 
